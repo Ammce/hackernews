769c7bf482613cf4ac2c127c569fd0cad3700d7c
Amel Muminovic <amcenp@gmail.com>
2022-03-13 01:10:12 +0100

    it works

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -9,18 +9,39 @@ import (
 
 	"github.com/Ammce/hackernews/graph/generated"
 	"github.com/Ammce/hackernews/graph/model"
+	"github.com/Ammce/hackernews/models"
 )
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	return &model.Todo{
-		ID:   "abc-123",
-		Text: "hello",
-		Done: false,
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*models.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*models.Todo, error) {
+	return []*models.Todo{
+		{
+			ID:   "abc-123",
+			Text: "hello",
+			Done: false,
+		},
 	}, nil
 }
 
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+func (r *todoResolver) User(ctx context.Context, obj *models.Todo) (*models.User, error) {
+	fmt.Println(obj)
+	return &models.User{
+		Name: "Amel",
+		ID:   "224",
+	}, nil
+}
+
+func (r *userResolver) Todos(ctx context.Context, obj *models.User) ([]*models.Todo, error) {
+	return []*models.Todo{
+		{
+			ID:   "abc-224",
+			Text: "hello",
+			Done: false,
+		},
+	}, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
@@ -29,5 +50,13 @@ func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResol
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+// Todo returns generated.TodoResolver implementation.
+func (r *Resolver) Todo() generated.TodoResolver { return &todoResolver{r} }
+
+// User returns generated.UserResolver implementation.
+func (r *Resolver) User() generated.UserResolver { return &userResolver{r} }
+
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+type todoResolver struct{ *Resolver }
+type userResolver struct{ *Resolver }
