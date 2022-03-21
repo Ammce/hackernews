9ed59997191baad159db8a08eed4330317ec388b
Amel Muminovic <amcenp@gmail.com>
2022-03-21 09:57:27 +0100

    feat: some new migrations

---
+++ adapters/postgres/migrations/20220320233926_create_news.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE news (
+    id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    text TEXT,
+    published BOOLEAN,
+    created_by_id INTEGER REFERENCES users(id),
+    approved_by_id INTEGER REFERENCES users(id),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    deleted_at TIMESTAMP WITH TIME ZONE DEFAULT NULL
+)
