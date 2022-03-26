5ed6c275521194512bdfa0d4c9725fdc6c49f727
Amel Muminovic <amcenp@gmail.com>
2022-03-26 21:09:28 +0100

    feat: refactor on news started

--- domain/news/newsEntity.go
+++ domain/news/newsEntity.go
@@ -1,1 +1,11 @@
 package news
+
+type News struct {
+	ID           string
+	Title        string
+	Text         string
+	Published    bool
+	CreatedById  string
+	CreatedAt    string
+	ApprovedById string
+}
---
+++ domain/role/roleEntity.go
@@ -0,0 +1,6 @@
+package role
+
+type Role struct {
+	ID   string `json:"id"`
+	Name string `json:"string"`
+}
